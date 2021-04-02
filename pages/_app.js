bc7fbd135f675dd59320ac2486055a1765feb7aa
svrem125 <svrem@icloud.com>
2021-04-02 14:27:39 +0200

    added a meta description

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="Description"
+          content="Game about click speed. Get as many points as you can with out touching the wite tiles!"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
