d6feb80a0d9eb0ac621eebb94988dd312b3aa8ca
svrem125 <svrem@icloud.com>
2021-04-02 14:32:21 +0200

    test

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,10 @@ function MyApp({ Component, pageProps }) {
           name="Description"
           content="Game about click speed. Get as many points as you can with out touching the wite tiles!"
         />
+        <meta
+          name="google-site-verification"
+          content="lubxIz_LGhY4WO7M8_2JPXvNX-ZN6ar49dfL6VzkU3s"
+        />
       </Head>
       <Component {...pageProps} />
     </>
